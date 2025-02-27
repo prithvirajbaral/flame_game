322ba4cf6ee435296e82aab6fdfdbf855cf59ac9
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 17:56:13 +0530

    add obstacle sprites and implement obstacle behavior in GoGreenWorld

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,5 @@
 const gameWidth = 1080.0;
 const gameHeight = 2000.0;
+
+const obstacleSize = 216.0;
+const extendedHeight = gameHeight * 4;
--- lib/game/go_green_world.dart
+++ lib/game/go_green_world.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame_game/go_green.dart';
+import 'package:flame_game/constants.dart';
+import 'package:flame_game/game/go_green_game.dart';
 import 'package:flame_game/sprites/bin.dart';
+import 'package:flame_game/sprites/obstacle.dart';
 import 'package:flame_game/sprites/player.dart';
 
-class GoGreenWorld extends World with HasGameRef<GoGreen>, CollisionCallbacks {
+class GoGreenWorld extends World with HasGameRef<GoGreenGame>, CollisionCallbacks {
   late final Player player;
   @override
   FutureOr<void> onLoad() {
@@ -14,5 +16,22 @@ class GoGreenWorld extends World with HasGameRef<GoGreen>, CollisionCallbacks {
     player = Player();
     add(player);
     add(Bin());
+
+    add(ObstacleTrash()..position = Vector2(0, 0));
+    add(ObstacleFire()..position = Vector2(-obstacleSize * 2, 0));
+    add(ObstacleWater()..position = Vector2(obstacleSize * 2, 0));
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    children.whereType<Obstacle>().forEach((obstacle) {
+      obstacle.position.y -= (dt * 400);
+
+      if (obstacle.position.y < -(gameRef.size.y / 2)) {
+        obstacle.position.y = extendedHeight;
+      }
+    });
   }
 }
