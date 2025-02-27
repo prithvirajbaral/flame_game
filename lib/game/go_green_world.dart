84037c377ffd5b4edf6759780c208e8c9a5af790
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 17:20:38 +0530

    add collision support to GoGreenWorld by implementing CollisionCallbacks

--- lib/game/go_green_world.dart
+++ lib/game/go_green_world.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame_game/go_green.dart';
 import 'package:flame_game/sprites/bin.dart';
 import 'package:flame_game/sprites/player.dart';
 
-class GoGreenWorld extends World with HasGameRef<GoGreen> {
+class GoGreenWorld extends World with HasGameRef<GoGreen>, CollisionCallbacks {
   late final Player player;
   @override
   FutureOr<void> onLoad() {
