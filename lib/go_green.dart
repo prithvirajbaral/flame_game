3803a31c0e3b72a6ac9b6e685b8cab51b22bf917
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 12:21:12 +0530

    add player instances to GoGreen game on load

--- lib/go_green.dart
+++ lib/go_green.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
 import 'package:flame/game.dart';
+import 'package:flame_game/player.dart';
 import 'package:flutter/material.dart';
 
 class GoGreen extends FlameGame {
   GoGreen({super.children});
+
+  @override
+  FutureOr<void> onLoad() {
+    super.onLoad();
+    world.add(Player(position: Vector2(0, 0), radius: 50.0));
+    world.add(Player(position: Vector2(0, 100), radius: 20.0, color: Colors.black));
+  }
 
   @override
   Color backgroundColor() {
