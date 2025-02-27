cfb63986c4c99ec131dd83b56190d60a9351cce7
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 14:31:49 +0530

    initialize camera with fixed resolution and adjust player instances in GoGreen game

--- lib/go_green.dart
+++ lib/go_green.dart
@@ -1,19 +1,32 @@
 import 'dart:async';
 
+import 'package:flame/camera.dart';
 import 'package:flame/game.dart';
 import 'package:flame_game/constants.dart';
 import 'package:flame_game/player.dart';
 import 'package:flutter/material.dart';
 
 class GoGreen extends FlameGame {
-  GoGreen({super.children});
+  GoGreen()
+    : super(
+        camera: CameraComponent.withFixedResolution(
+          width: gameWidth,
+          height: gameHeight,
+        ),
+      );
 
   @override
   FutureOr<void> onLoad() {
     super.onLoad();
-    world.add(Player(position: Vector2(-gameWidth / 4, 0), radius: gameWidth / 4));
     world.add(
-      Player(position: Vector2(gameWidth / 4, 0), radius: gameWidth / 4, color: Colors.black),
+      Player(position: Vector2(-gameWidth / 4, 0), radius: gameWidth / 4),
+    );
+    world.add(
+      Player(
+        position: Vector2(gameWidth / 4, 0),
+        radius: gameWidth / 2,
+        color: Colors.black,
+      ),
     );
   }
 
