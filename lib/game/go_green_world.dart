156947cba0499176d2f8827f3aebecd51b2ef3de
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 15:47:09 +0530

    move Player class to sprites directory and implement vertical movement with clamping for boundaries

--- lib/game/go_green_world.dart
+++ lib/game/go_green_world.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_game/go_green.dart';
-import 'package:flame_game/player.dart';
+import 'package:flame_game/sprites/player.dart';
 
 class GoGreenWorld extends World with HasGameRef<GoGreen> {
   late final Player player;
