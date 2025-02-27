35afef9dd973f919b45d2adc463f9bab30f5ce63
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 15:42:13 +0530

    initialize Player instance in GoGreenWorld on load

--- lib/game/go_green_world.dart
+++ lib/game/go_green_world.dart
@@ -5,9 +5,11 @@ import 'package:flame_game/go_green.dart';
 import 'package:flame_game/player.dart';
 
 class GoGreenWorld extends World with HasGameRef<GoGreen> {
+  late final Player player;
   @override
   FutureOr<void> onLoad() {
     super.onLoad();
-    add(Player());  
+    player = Player();
+    add(player);
   }
 }
