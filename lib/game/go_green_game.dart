6df4535707a42089aa3cac49ebc51cb4c2e0e36b
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 15:41:58 +0530

    implement horizontal drag detection in GoGreenGame and change background color to yellow

--- lib/game/go_green_game.dart
+++ lib/game/go_green_game.dart
@@ -1,11 +1,11 @@
-
 import 'package:flame/camera.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_game/constants.dart';
 import 'package:flame_game/game/go_green_world.dart';
 import 'package:flutter/material.dart';
 
-class GoGreenGame extends FlameGame {
+class GoGreenGame extends FlameGame<GoGreenWorld> with HorizontalDragDetector {
   GoGreenGame()
     : super(
         world: GoGreenWorld(),
@@ -15,10 +15,14 @@ class GoGreenGame extends FlameGame {
         ),
       );
 
-  
-
   @override
   Color backgroundColor() {
-    return Colors.green;
+    return Colors.yellow;
+  }
+
+  @override
+  void onHorizontalDragUpdate(DragUpdateInfo info) {
+    super.onHorizontalDragUpdate(info);
+    world.player.move(info.delta.global.x);
   }
 }
