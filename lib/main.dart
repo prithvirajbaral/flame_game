0c5cdd81b3ddbd567af153e20ef84d6f925e64b7
prithvirajbaral <prithvirajbaral@gmail.com>
2025-02-27 12:47:11 +0530

    add GameApp widget to encapsulate GoGreen game initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:flame/game.dart';
-import 'package:flame_game/go_green.dart';
+import 'package:flame_game/game_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = GoGreen();
   runApp(
-    GameWidget(game: game),
+    GameApp(),
   );
 }
 
